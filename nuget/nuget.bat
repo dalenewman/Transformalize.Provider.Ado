0f98edfc0ce2bbcfbf2d651326b9e35a24e0dc64
Dale Newman <dalenewman@gmail.com>
2018-12-10 17:02:04 -0500

    Initial commit

---
+++ nuget/nuget.bat
@@ -0,0 +1,11 @@
+nuget pack Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules"
+nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules"
+
+REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.3.13-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.3.13-beta.nupkg" -source https://api.nuget.org/v3/index.json
+
+
+
+
+
+
