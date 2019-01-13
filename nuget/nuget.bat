8a4424112bc21e4b72f5082e3f137fa31632171e
Dale Newman <dalenewman@gmail.com>
2019-01-13 00:06:13 -0500

    update nuget

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -2,9 +2,9 @@ nuget pack Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.3.14-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
