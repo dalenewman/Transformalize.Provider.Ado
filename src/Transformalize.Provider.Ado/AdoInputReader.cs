1ea2dfd0cf3e3ec1849cd61579c2ab1b7f0e08ac
Dale Newman <dalenewman@gmail.com>
2019-07-02 17:05:30 -0400

    added support for ado style parameters in filters (when paging)

--- src/Transformalize.Provider.Ado/AdoInputReader.cs
+++ src/Transformalize.Provider.Ado/AdoInputReader.cs
@@ -28,115 +28,122 @@ using Transformalize.Contracts;
 using Transformalize.Providers.Ado.Ext;
 
 namespace Transformalize.Providers.Ado {
-    /// <summary>
-    /// A reader for an entity's input (source).
-    /// </summary>
-    public class AdoInputReader : IRead {
+   /// <summary>
+   /// A reader for an entity's input (source).
+   /// </summary>
+   public class AdoInputReader : IRead {
 
-        private int _rowCount;
-        private readonly InputContext _input;
-        private readonly AdoRowCreator _rowCreator;
-        private readonly Field[] _fields;
-        private readonly IConnectionFactory _factory;
+      private int _rowCount;
+      private readonly InputContext _input;
+      private readonly AdoRowCreator _rowCreator;
+      private readonly Field[] _fields;
+      private readonly IConnectionFactory _factory;
 
-        public AdoInputReader(InputContext input, Field[] fields, IConnectionFactory factory, IRowFactory rowFactory) {
-            _input = input;
-            _fields = fields;
-            _factory = factory;
-            _rowCreator = new AdoRowCreator(input, rowFactory);
-        }
+      public AdoInputReader(InputContext input, Field[] fields, IConnectionFactory factory, IRowFactory rowFactory) {
+         _input = input;
+         _fields = fields;
+         _factory = factory;
+         _rowCreator = new AdoRowCreator(input, rowFactory);
+      }
 
-        public IEnumerable<IRow> Read() {
+      public IEnumerable<IRow> Read() {
 
-            using (var cn = _factory.GetConnection()) {
+         using (var cn = _factory.GetConnection(Constants.ApplicationName)) {
 
-                try {
-                    cn.Open();
-                } catch (DbException e) {
-                    _input.Error($"Can't open {_input.Connection} for reading.");
-                    _input.Error(e.Message);
-                    yield break;
-                }
-
-                var cmd = cn.CreateCommand();
-
-                if (string.IsNullOrEmpty(_input.Entity.Query)) {
-                    if (_input.Entity.MinVersion == null) {
-                        cmd.CommandText = _input.SqlSelectInput(_fields, _factory);
-                        _input.Debug(() => cmd.CommandText);
-                    } else {
-                        cmd.CommandText = _input.SqlSelectInputWithMinVersion(_fields, _factory);
-                        _input.Debug(() => cmd.CommandText);
-
-                        var parameter = cmd.CreateParameter();
-                        parameter.ParameterName = "@MinVersion";
-                        parameter.Direction = ParameterDirection.Input;
-                        parameter.Value = _input.Entity.MinVersion;
-                        cmd.Parameters.Add(parameter);
-                    }
-
-                    if (_input.Entity.IsPageRequest()) {
-                        var sql = $"SELECT COUNT(*) FROM {_input.SqlInputName(_factory)} {(_factory.AdoProvider == AdoProvider.SqlServer ? "WITH (NOLOCK)" : string.Empty)} {(_input.Entity.Filter.Any() ? " WHERE " + _input.ResolveFilter(_factory) : string.Empty)}";
-                        _input.Debug(() => sql);
-                        try {
-                            _input.Entity.Hits = cn.ExecuteScalar<int>(sql);
-                        } catch (Exception ex) {
-                            _input.Error(ex.Message);
-                        }
-                    }
-                    _input.Entity.Query = cmd.CommandText;
-                } else {
-                    if (_input.Entity.Query.Length <= 128 && _input.Process.Scripts.Any(s => s.Name == _input.Entity.Query)) {
-                        var script = _input.Process.Scripts.First(s => s.Name == _input.Entity.Query);
-                        if (script.Content != string.Empty) {
-                            _input.Entity.Query = script.Content;
-                        }
-                    }
-                    cmd.CommandText = _input.Entity.Query;
-                }
-
-                // handle ado parameters
-                if (cmd.CommandText.Contains("@")) {
-                    var active = _input.Process.Parameters;
-                    foreach (var name in new AdoParameterFinder().Find(cmd.CommandText).Distinct().ToList()) {
-                        var match = active.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-                        if (match != null) {
-                            var parameter = cmd.CreateParameter();
-                            parameter.ParameterName = match.Name;
-                            parameter.Value = match.Convert(match.Value);
-                            cmd.Parameters.Add(parameter);
-                        }
-                    }
-                }
-                cmd.CommandType = CommandType.Text;
-                cmd.CommandTimeout = 0;
-
-                IDataReader reader;
-
-                try {
-                    reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess);
-                } catch (DbException ex) {
-                    _input.Error(ex.Message);
-                    yield break;
-                }
-
-                using (reader) {
-
-                    if (_fields.Length < reader.FieldCount) {
-                        _input.Warn($"The reader is returning {reader.FieldCount} fields, but the entity {_input.Entity.Alias} expects {_fields.Length}!");
-                    }
-
-                    // just read
-                    while (reader.Read()) {
-                        _rowCount++;
-                        yield return _rowCreator.Create(reader, _fields);
-                    }
-
-                }
+            try {
+               cn.Open();
+            } catch (DbException e) {
+               _input.Error($"Can't open {_input.Connection} for reading.");
+               _input.Error(e.Message);
+               yield break;
             }
 
-            _input.Info("{0} from {1}", _rowCount, _input.Connection.Name);
-        }
-    }
+            var cmd = cn.CreateCommand();
+
+            if (string.IsNullOrEmpty(_input.Entity.Query)) {
+               if (_input.Entity.MinVersion == null) {
+                  cmd.CommandText = _input.SqlSelectInput(_fields, _factory);
+                  _input.Debug(() => cmd.CommandText);
+               } else {
+                  cmd.CommandText = _input.SqlSelectInputWithMinVersion(_fields, _factory);
+                  _input.Debug(() => cmd.CommandText);
+
+                  var parameter = cmd.CreateParameter();
+                  parameter.ParameterName = "@MinVersion";
+                  parameter.Direction = ParameterDirection.Input;
+                  parameter.Value = _input.Entity.MinVersion;
+                  cmd.Parameters.Add(parameter);
+               }
+
+               if (_input.Entity.IsPageRequest()) {
+                  var countCmd = cn.CreateCommand();
+                  countCmd.CommandText = $"SELECT COUNT(*) FROM {_input.SqlInputName(_factory)} {(_factory.AdoProvider == AdoProvider.SqlServer ? "WITH (NOLOCK)" : string.Empty)} {(_input.Entity.Filter.Any() ? " WHERE " + _input.ResolveFilter(_factory) : string.Empty)}";
+                  _input.Debug(() => countCmd.CommandText);
+                  AddAdoParameters(countCmd);
+                  try {
+                     _input.Entity.Hits = (int) countCmd.ExecuteScalar();
+                  } catch (DbException ex) {
+                     _input.Error($"Error counting {_input.Entity.Name} records.");
+                     _input.Error(ex.Message);
+                  }
+               }
+               _input.Entity.Query = cmd.CommandText;
+            } else {
+               if (_input.Entity.Query.Length <= 128 && _input.Process.Scripts.Any(s => s.Name == _input.Entity.Query)) {
+                  var script = _input.Process.Scripts.First(s => s.Name == _input.Entity.Query);
+                  if (script.Content != string.Empty) {
+                     _input.Entity.Query = script.Content;
+                  }
+               }
+               cmd.CommandText = _input.Entity.Query;
+            }
+
+            // handle ado parameters
+            AddAdoParameters(cmd);
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandTimeout = _input.Connection.RequestTimeout;
+
+            IDataReader reader;
+
+            try {
+               reader = cmd.ExecuteReader(CommandBehavior.SequentialAccess);
+            } catch (DbException ex) {
+               _input.Error(ex.Message);
+               yield break;
+            }
+
+            using (reader) {
+
+               if (_fields.Length < reader.FieldCount) {
+                  _input.Warn($"The reader is returning {reader.FieldCount} fields, but the entity {_input.Entity.Alias} expects {_fields.Length}!");
+               }
+
+               // just read
+               while (reader.Read()) {
+                  _rowCount++;
+                  yield return _rowCreator.Create(reader, _fields);
+               }
+
+            }
+         }
+
+         _input.Info("{0} from {1}", _rowCount, _input.Connection.Name);
+      }
+
+      public void AddAdoParameters(IDbCommand cmd) {
+         if (cmd.CommandText.Contains("@")) {
+            var active = _input.Process.Parameters;
+            foreach (var name in new AdoParameterFinder().Find(cmd.CommandText).Distinct().ToList()) {
+               var match = active.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+               if (match != null) {
+                  var parameter = cmd.CreateParameter();
+                  parameter.ParameterName = match.Name;
+                  parameter.Value = match.Convert(match.Value);
+                  cmd.Parameters.Add(parameter);
+               }
+            }
+         }
+      }
+   }
 
 }
