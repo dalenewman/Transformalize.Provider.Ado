5c0dba600f7df7de8eaf830118eb9968cb62a340
Dale Newman <dalenewman@gmail.com>
2019-09-26 09:55:54 -0400

    handle DBNull.Value for min/max versions

--- src/Transformalize.Provider.Ado/AdoInputVersionDetector.cs
+++ src/Transformalize.Provider.Ado/AdoInputVersionDetector.cs
@@ -26,78 +26,79 @@ using Transformalize.Contracts;
 using Transformalize.Providers.Ado.Ext;
 
 namespace Transformalize.Providers.Ado {
-    public class AdoInputProvider : IInputProvider {
-        private readonly InputContext _context;
-        private readonly IConnectionFactory _cf;
+   public class AdoInputProvider : IInputProvider {
+      private readonly InputContext _context;
+      private readonly IConnectionFactory _cf;
 
-        public AdoInputProvider(InputContext context, IConnectionFactory connectionFactory) {
-            _context = context;
-            _cf = connectionFactory;
-        }
+      public AdoInputProvider(InputContext context, IConnectionFactory connectionFactory) {
+         _context = context;
+         _cf = connectionFactory;
+      }
 
-        public object GetMaxVersion() {
+      public object GetMaxVersion() {
 
-            if (string.IsNullOrEmpty(_context.Entity.Version))
-                return null;
+         if (string.IsNullOrEmpty(_context.Entity.Version))
+            return null;
 
-            var version = _context.Entity.GetVersionField();
+         var version = _context.Entity.GetVersionField();
 
-            var schema = _context.Entity.Schema == string.Empty ? string.Empty : _cf.Enclose(_context.Entity.Schema) + ".";
+         var schema = _context.Entity.Schema == string.Empty ? string.Empty : _cf.Enclose(_context.Entity.Schema) + ".";
 
-            var filter = string.Empty;
-            if (_context.Entity.Filter.Any()) {
-                filter = _context.ResolveFilter(_cf);
+         var filter = string.Empty;
+         if (_context.Entity.Filter.Any()) {
+            filter = _context.ResolveFilter(_cf);
+         }
+
+         string sql;
+         if (_context.Connection.Provider == "sqlserver" && version.Type == "byte[]" && version.Length == "8") {
+            sql = $"SELECT MAX({_cf.Enclose(version.Name)}) FROM {schema}{_cf.Enclose(_context.Entity.Name)} WHERE {_cf.Enclose(version.Name)} < MIN_ACTIVE_ROWVERSION() {(filter == string.Empty ? string.Empty : " AND " + filter)}";
+         } else {
+            sql = $"SELECT MAX({_cf.Enclose(version.Name)}) FROM {schema}{_cf.Enclose(_context.Entity.Name)} {(filter == string.Empty ? string.Empty : " WHERE " + filter)}";
+         }
+
+         _context.Debug(() => $"Loading Input Version: {sql}");
+
+         try {
+            using (var cn = _cf.GetConnection()) {
+               cn.Open();
+
+               var cmd = cn.CreateCommand();
+               cmd.CommandText = sql;
+               cmd.CommandType = CommandType.Text;
+               cmd.CommandTimeout = _context.Connection.RequestTimeout;
+
+               // handle ado parameters
+               if (cmd.CommandText.Contains("@")) {
+                  var active = _context.Process.Parameters;
+                  foreach (var name in new AdoParameterFinder().Find(cmd.CommandText).Distinct().ToList()) {
+                     var match = active.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+                     if (match != null) {
+                        var parameter = cmd.CreateParameter();
+                        parameter.ParameterName = match.Name;
+                        parameter.Value = match.Convert(match.Value);
+                        cmd.Parameters.Add(parameter);
+                     }
+                  }
+               }
+
+               var result = cmd.ExecuteScalar();
+               return result == DBNull.Value ? null : result;
             }
+         } catch (System.Data.Common.DbException ex) {
+            _context.Error($"Error retrieving max version from {_context.Connection.Name}, {_context.Entity.Alias}.");
+            _context.Error(ex.Message);
+            _context.Debug(() => ex.StackTrace);
+            _context.Debug(() => sql);
+            return null;
+         }
+      }
 
-            string sql;
-            if (_context.Connection.Provider == "sqlserver" && version.Type == "byte[]" && version.Length == "8") {
-                sql = $"SELECT MAX({_cf.Enclose(version.Name)}) FROM {schema}{_cf.Enclose(_context.Entity.Name)} WHERE {_cf.Enclose(version.Name)} < MIN_ACTIVE_ROWVERSION() {(filter == string.Empty ? string.Empty : " AND " + filter)}";
-            } else {
-                sql = $"SELECT MAX({_cf.Enclose(version.Name)}) FROM {schema}{_cf.Enclose(_context.Entity.Name)} {(filter == string.Empty ? string.Empty : " WHERE " + filter)}";
-            }
+      public Schema GetSchema(Entity entity = null) {
+         throw new NotImplementedException();
+      }
 
-            _context.Debug(() => $"Loading Input Version: {sql}");
-
-            try {
-                using (var cn = _cf.GetConnection()) {
-                    cn.Open();
-
-                    var cmd = cn.CreateCommand();
-                    cmd.CommandText = sql;
-                    cmd.CommandType = CommandType.Text;
-                    cmd.CommandTimeout = _context.Connection.RequestTimeout;
-
-                    // handle ado parameters
-                    if (cmd.CommandText.Contains("@")) {
-                        var active = _context.Process.Parameters;
-                        foreach (var name in new AdoParameterFinder().Find(cmd.CommandText).Distinct().ToList()) {
-                            var match = active.FirstOrDefault(p => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-                            if (match != null) {
-                                var parameter = cmd.CreateParameter();
-                                parameter.ParameterName = match.Name;
-                                parameter.Value = match.Convert(match.Value);
-                                cmd.Parameters.Add(parameter);
-                            }
-                        }
-                    }
-
-                    return cmd.ExecuteScalar();
-                }
-            } catch (System.Data.Common.DbException ex) {
-                _context.Error($"Error retrieving max version from {_context.Connection.Name}, {_context.Entity.Alias}.");
-                _context.Error(ex.Message);
-                _context.Debug(() => ex.StackTrace);
-                _context.Debug(() => sql);
-                return null;
-            }
-        }
-
-        public Schema GetSchema(Entity entity = null) {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<IRow> Read() {
-            throw new NotImplementedException();
-        }
-    }
+      public IEnumerable<IRow> Read() {
+         throw new NotImplementedException();
+      }
+   }
 }
--- src/Transformalize.Provider.Ado/AdoOutputProvider.cs
+++ src/Transformalize.Provider.Ado/AdoOutputProvider.cs
@@ -27,177 +27,178 @@ using Transformalize.Providers.Ado.Ext;
 
 namespace Transformalize.Providers.Ado {
 
-    public class AdoOutputProvider : IOutputProvider {
+   public class AdoOutputProvider : IOutputProvider {
 
-        private readonly OutputContext _context;
-        private readonly IConnectionFactory _cf;
-        private readonly IDbConnection _cn;
-        private readonly IWrite _writer;
+      private readonly OutputContext _context;
+      private readonly IConnectionFactory _cf;
+      private readonly IDbConnection _cn;
+      private readonly IWrite _writer;
 
-        public AdoOutputProvider(OutputContext context, IConnectionFactory cf, IWrite writer) {
-            _writer = writer;
-            _context = context;
-            _cf = cf;
-            _cn = cf.GetConnection();
-        }
+      public AdoOutputProvider(OutputContext context, IConnectionFactory cf, IWrite writer) {
+         _writer = writer;
+         _context = context;
+         _cf = cf;
+         _cn = cf.GetConnection();
+      }
 
-        public void Delete() {
-            throw new NotImplementedException();
-        }
+      public void Delete() {
+         throw new NotImplementedException();
+      }
 
-        public object GetMaxVersion() {
+      public object GetMaxVersion() {
 
-            if (string.IsNullOrEmpty(_context.Entity.Version))
-                return null;
+         if (string.IsNullOrEmpty(_context.Entity.Version))
+            return null;
 
-            var version = _context.Entity.GetVersionField();
-            string sql;
-            string objectName;
+         var version = _context.Entity.GetVersionField();
+         string sql;
+         string objectName;
 
-            switch (_cf.AdoProvider) {
-                case AdoProvider.PostgreSql:
-                    objectName = _cf.Enclose(_context.Entity.OutputViewName(_context.Process.Name));
-                    break;
-                case AdoProvider.SqlCe:
-                    objectName = _cf.Enclose(_context.Entity.OutputTableName(_context.Process.Name));
-                    break;
-                default:
-                    objectName = _cf.Enclose(_context.Entity.OutputViewName(_context.Process.Name));
-                    break;
+         switch (_cf.AdoProvider) {
+            case AdoProvider.PostgreSql:
+               objectName = _cf.Enclose(_context.Entity.OutputViewName(_context.Process.Name));
+               break;
+            case AdoProvider.SqlCe:
+               objectName = _cf.Enclose(_context.Entity.OutputTableName(_context.Process.Name));
+               break;
+            default:
+               objectName = _cf.Enclose(_context.Entity.OutputViewName(_context.Process.Name));
+               break;
+         }
+
+         switch (_cf.AdoProvider) {
+            case AdoProvider.PostgreSql:
+               sql = _context.Entity.Delete ? $"SELECT {_cf.Enclose(version.Alias)} FROM {objectName} WHERE {_cf.Enclose(Constants.TflDeleted)} = false ORDER BY {_cf.Enclose(version.Alias)} DESC LIMIT 1;" : $"SELECT {_cf.Enclose(version.Alias)} FROM {objectName} ORDER BY {_cf.Enclose(version.Alias)} DESC LIMIT 1;";
+               break;
+            case AdoProvider.SqlCe:
+               sql = _context.Entity.Delete ? $"SELECT MAX({_cf.Enclose(version.FieldName())}) FROM {objectName} WHERE {_cf.Enclose(_context.Entity.TflDeleted().FieldName())} = 0;" : $"SELECT MAX({_cf.Enclose(version.FieldName())}) FROM {objectName};";
+               break;
+            default:
+               sql = _context.Entity.Delete ? $"SELECT MAX({_cf.Enclose(version.Alias)}) FROM {objectName} WHERE {_cf.Enclose(Constants.TflDeleted)} = 0;" : $"SELECT MAX({_cf.Enclose(version.Alias)}) FROM {objectName};";
+               break;
+         }
+
+         _context.Debug(() => $"Loading Output Version: {sql}");
+
+         try {
+            if (_cn.State != ConnectionState.Open) {
+               _cn.Open();
             }
+            var result = _cn.ExecuteScalar(sql, commandTimeout: _context.Connection.RequestTimeout);
+            return result == DBNull.Value ? null : result;
+         } catch (System.Data.Common.DbException ex) {
+            _context.Error($"Error retrieving maximum version from {_context.Connection.Name}, {objectName}.");
+            _context.Error(ex.Message);
+            _context.Debug(() => ex.StackTrace);
+            _context.Debug(() => sql);
+            return null;
+         }
+      }
 
-            switch (_cf.AdoProvider) {
-                case AdoProvider.PostgreSql:
-                    sql = _context.Entity.Delete ? $"SELECT {_cf.Enclose(version.Alias)} FROM {objectName} WHERE {_cf.Enclose(Constants.TflDeleted)} = false ORDER BY {_cf.Enclose(version.Alias)} DESC LIMIT 1;" : $"SELECT {_cf.Enclose(version.Alias)} FROM {objectName} ORDER BY {_cf.Enclose(version.Alias)} DESC LIMIT 1;";
-                    break;
-                case AdoProvider.SqlCe:
-                    sql = _context.Entity.Delete ? $"SELECT MAX({_cf.Enclose(version.FieldName())}) FROM {objectName} WHERE {_cf.Enclose(_context.Entity.TflDeleted().FieldName())} = 0;" : $"SELECT MAX({_cf.Enclose(version.FieldName())}) FROM {objectName};";
-                    break;
-                default:
-                    sql = _context.Entity.Delete ? $"SELECT MAX({_cf.Enclose(version.Alias)}) FROM {objectName} WHERE {_cf.Enclose(Constants.TflDeleted)} = 0;" : $"SELECT MAX({_cf.Enclose(version.Alias)}) FROM {objectName};";
-                    break;
-            }
+      public void End() {
+         if (_context.Process.Mode != "init" || _context.Entity == null)
+            return;
 
-            _context.Debug(() => $"Loading Output Version: {sql}");
+         if (_context.Entity.Version != string.Empty) {
 
             try {
-                if (_cn.State != ConnectionState.Open) {
-                    _cn.Open();
-                }
-                return _cn.ExecuteScalar(sql, commandTimeout: _context.Connection.RequestTimeout);
+               if (_cn.State != ConnectionState.Open) {
+                  _cn.Open();
+               }
+
+               var version = _context.Entity.GetVersionField();
+               _cn.Execute(_context.SqlCreateVersionIndex(_cf, version), commandTimeout: _context.Connection.RequestTimeout);
+               _context.Debug(() => $"Indexed version for {_context.Entity.Alias}.");
+               _context.Debug(() => _context.SqlCreateVersionIndex(_cf, version));
+
+               if (_context.Entity.IsMaster) {
+                  _cn.Execute(_context.SqlCreateBatchIndex(_cf), commandTimeout: _context.Connection.RequestTimeout);
+                  _context.Debug(() => $"Indexed batch for {_context.Entity.Alias}.");
+                  _context.Debug(() => _context.SqlCreateBatchIndex(_cf));
+               }
+
             } catch (System.Data.Common.DbException ex) {
-                _context.Error($"Error retrieving maximum version from {_context.Connection.Name}, {objectName}.");
-                _context.Error(ex.Message);
-                _context.Debug(() => ex.StackTrace);
-                _context.Debug(() => sql);
-                return null;
+               _context.Warn($"Error creating index: {ex.Message}");
+               _context.Debug(() => ex.StackTrace);
+
             }
-        }
+         }
 
-        public void End() {
-            if (_context.Process.Mode != "init" || _context.Entity == null)
-                return;
-
-            if (_context.Entity.Version != string.Empty) {
-
-                try {
-                    if (_cn.State != ConnectionState.Open) {
-                        _cn.Open();
-                    }
-
-                    var version = _context.Entity.GetVersionField();
-                    _cn.Execute(_context.SqlCreateVersionIndex(_cf, version), commandTimeout: _context.Connection.RequestTimeout);
-                    _context.Debug(() => $"Indexed version for {_context.Entity.Alias}.");
-                    _context.Debug(() => _context.SqlCreateVersionIndex(_cf, version));
-
-                    if (_context.Entity.IsMaster) {
-                        _cn.Execute(_context.SqlCreateBatchIndex(_cf), commandTimeout: _context.Connection.RequestTimeout);
-                        _context.Debug(() => $"Indexed batch for {_context.Entity.Alias}.");
-                        _context.Debug(() => _context.SqlCreateBatchIndex(_cf));
-                    }
-
-                } catch (System.Data.Common.DbException ex) {
-                    _context.Warn($"Error creating index: {ex.Message}");
-                    _context.Debug(() => ex.StackTrace);
-
-                }
+         if (_context.Entity.RelationshipToMaster.Any()) {
+            var response = new AdoEntityDepthChecker(_context, _cf).Execute();
+            if (response.Code != 200) {
+               _context.Warn(response.Message);
             }
+         }
 
-            if (_context.Entity.RelationshipToMaster.Any()) {
-                var response = new AdoEntityDepthChecker(_context, _cf).Execute();
-                if (response.Code != 200) {
-                    _context.Warn(response.Message);
-                }
+      }
+
+      public int GetNextTflBatchId() {
+         if (_cn.State != ConnectionState.Open) {
+            _cn.Open();
+         }
+         var sql = _context.SqlControlLastBatchId(_cf);
+
+         var result = 0;
+         try {
+            result = _cn.ExecuteScalar<int>(sql) + 1;
+         } catch (System.Data.Common.DbException e) {
+            _context.Error(e.Message);
+            _context.Debug(() => sql);
+            _context.Debug(() => e.StackTrace);
+         }
+
+         return result;
+
+      }
+
+      public int GetMaxTflKey() {
+         if (_cn.State != ConnectionState.Open) {
+            _cn.Open();
+         }
+
+         var tableName = _context.Entity.OutputTableName(_context.Process.Name);
+         var sql = $"SELECT MAX({_cf.Enclose(_context.Entity.TflKey().FieldName())}) FROM {_cf.Enclose(tableName)};";
+         var result = 0;
+         try {
+            result = _cn.ExecuteScalar<int>(sql);
+         } catch (System.Data.Common.DbException e) {
+            _context.Error($"Error retrieving maximum surrogate key (TflKey) from {tableName}.");
+            _context.Error(e.Message);
+            _context.Debug(() => sql);
+            _context.Debug(() => e.StackTrace);
+         }
+
+         return result;
+      }
+
+      public void Initialize() {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IRow> Match(IEnumerable<IRow> rows) {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<IRow> ReadKeys() {
+         throw new NotImplementedException();
+      }
+
+      public void Start() {
+         throw new NotImplementedException();
+      }
+
+      public void Write(IEnumerable<IRow> rows) {
+         _writer.Write(rows);
+      }
+
+      public void Dispose() {
+         if (_cn != null) {
+            if (_cn.State != ConnectionState.Closed) {
+               _cn.Close();
             }
-
-        }
-
-        public int GetNextTflBatchId() {
-            if (_cn.State != ConnectionState.Open) {
-                _cn.Open();
-            }
-            var sql = _context.SqlControlLastBatchId(_cf);
-
-            var result = 0;
-            try {
-                result = _cn.ExecuteScalar<int>(sql) + 1;
-            } catch (System.Data.Common.DbException e) {
-                _context.Error(e.Message);
-                _context.Debug(() => sql);
-                _context.Debug(() => e.StackTrace);
-            }
-
-            return result;
-
-        }
-
-        public int GetMaxTflKey() {
-            if (_cn.State != ConnectionState.Open) {
-                _cn.Open();
-            }
-
-            var tableName = _context.Entity.OutputTableName(_context.Process.Name);
-            var sql = $"SELECT MAX({_cf.Enclose(_context.Entity.TflKey().FieldName())}) FROM {_cf.Enclose(tableName)};";
-            var result = 0;
-            try {
-                result = _cn.ExecuteScalar<int>(sql);
-            } catch (System.Data.Common.DbException e) {
-                _context.Error($"Error retrieving maximum surrogate key (TflKey) from {tableName}.");
-                _context.Error(e.Message);
-                _context.Debug(() => sql);
-                _context.Debug(() => e.StackTrace);
-            }
-
-            return result;
-        }
-
-        public void Initialize() {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<IRow> Match(IEnumerable<IRow> rows) {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<IRow> ReadKeys() {
-            throw new NotImplementedException();
-        }
-
-        public void Start() {
-            throw new NotImplementedException();
-        }
-
-        public void Write(IEnumerable<IRow> rows) {
-            _writer.Write(rows);
-        }
-
-        public void Dispose() {
-            if (_cn != null) {
-                if (_cn.State != ConnectionState.Closed) {
-                    _cn.Close();
-                }
-                _cn.Dispose();
-            }
-        }
-    }
+            _cn.Dispose();
+         }
+      }
+   }
 }
