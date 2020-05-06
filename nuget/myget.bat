d8274e6557af530f7d4f7f37e82d19622e565c69
Dale Newman <dalenewman@gmail.com>
2020-05-06 11:21:04 -0400

    updated transformalize dependency and nuget

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -1,14 +1,14 @@
-REM nuget pack Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules" 
-REM nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
-REM 
-REM nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules" 
-REM nuget pack Transformalize.Transform.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
+nuget pack Transformalize.Provider.Ado.nuspec -OutputDirectory "c:\temp\modules" 
+nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM 
-REM nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.7.4-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules" 
+nuget pack Transformalize.Transform.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
+
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.7.6-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
