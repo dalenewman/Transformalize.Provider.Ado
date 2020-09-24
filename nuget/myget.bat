7c128356e3ce556a39c9861e6c9f35a25511d160
Dale Newman <dalenewman@gmail.com>
2020-09-23 23:54:32 -0400

    Trying out using the format attribute when reading dates and decimals from the database into un-typed (strings) in the arrangement.

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -4,11 +4,11 @@ REM nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\t
 REM nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules" 
 REM nuget pack Transformalize.Transform.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
  
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.8.30-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.8.30-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
  
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.8.15-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.8.26-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.8.26-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
 
 
 
--- src/Transformalize.Provider.Ado.Shared/AdoRowCreator.cs
+++ src/Transformalize.Provider.Ado.Shared/AdoRowCreator.cs
@@ -25,80 +25,91 @@ using Transformalize.Contracts;
 
 namespace Transformalize.Providers.Ado {
 
-    public class AdoRowCreator {
+   public class AdoRowCreator {
 
-        private readonly IConnectionContext _context;
-        private readonly IRowFactory _rowFactory;
-        private bool[] _errors;
-        private readonly Dictionary<string, Type> _typeMap;
-        private List<Func<object, object>> _conversions;
-        private int _fieldCount;
+      private readonly IConnectionContext _context;
+      private readonly IRowFactory _rowFactory;
+      private bool[] _errors;
+      private readonly Dictionary<string, Type> _typeMap;
+      private List<Func<int,object, object>> _conversions;
+      private int _fieldCount;
 
-        public AdoRowCreator(IConnectionContext context, IRowFactory rowFactory) {
-            _errors = null;
-            _context = context;
-            _rowFactory = rowFactory;
-            _typeMap = Constants.TypeSystem();
-        }
+      public AdoRowCreator(IConnectionContext context, IRowFactory rowFactory) {
+         _errors = null;
+         _context = context;
+         _rowFactory = rowFactory;
+         _typeMap = Constants.TypeSystem();
+      }
 
-        public IRow Create(IDataReader reader, Field[] fields) {
+      public IRow Create(IDataReader reader, Field[] fields) {
 
-            var row = _rowFactory.Create();
+         var row = _rowFactory.Create();
 
-            if (_fieldCount == 0) {
-                _fieldCount = Math.Min(reader.FieldCount, fields.Length);
-                _conversions = new List<Func<object, object>>(_fieldCount);
-                for (var i = 0; i < _fieldCount; i++) {
-                    _conversions.Add(null);
-                }
-                _errors = new bool[fields.Length];
-                for (var i = 0; i < _fieldCount; i++) {
-                    var inputType = reader.GetFieldType(i);
-                    _errors[i] = inputType != _typeMap[fields[i].Type];
+         if (_fieldCount == 0) {
+            _fieldCount = Math.Min(reader.FieldCount, fields.Length);
+            _conversions = new List<Func<int, object, object>>(_fieldCount);
+            for (var i = 0; i < _fieldCount; i++) {
+               _conversions.Add(null);
+            }
+            _errors = new bool[fields.Length];
+            for (var i = 0; i < _fieldCount; i++) {
+               var inputType = reader.GetFieldType(i);
+               _errors[i] = inputType != _typeMap[fields[i].Type];
 
-                    if (_errors[i]) {
-                        if (fields[i].Transforms.Any() && fields[i].Transforms.First().Method == "convert") {
-                            _conversions[i] = o => o;  // the user has set a conversion
+               if (_errors[i]) {
+                  if (fields[i].Transforms.Any() && fields[i].Transforms.First().Method == "convert") {
+                     _conversions[i] = (x,o) => o;  // the user has set a conversion
+                  } else {
+                     if(fields[i].Format != string.Empty && _typeMap[fields[i].Type] == typeof(string)) {
+                        if(inputType == typeof(DateTime)) {
+                           _conversions[i] = (x,o) => ((DateTime)o).ToString(fields[x].Format);
+                        } else if (inputType == typeof(decimal)) {
+                           _conversions[i] = (x,o) => ((decimal)o).ToString(fields[x].Format);
                         } else {
-                            _conversions[i] = fields[i].Convert;
-                            _context.Warn("Type mismatch for {0}. Expected {1}, but read {2}.  Change type or add conversion.", fields[i].Name, fields[i].Type, inputType);
+                           _conversions[i] = (x,o) => fields[x].Convert(o);
+                           _context.Warn("Type mismatch for {0}. Expected {1}, but read {2}.  Change type or add conversion.", fields[i].Name, fields[i].Type, inputType);
                         }
+                     } else {
+                        _conversions[i] = (x, o) => fields[x].Convert(o);
+                        _context.Warn("Type mismatch for {0}. Expected {1}, but read {2}.  Change type or add conversion.", fields[i].Name, fields[i].Type, inputType);
+                     }
+                  }
 
-                    } else {
-                        _conversions[i] = o => o;
-                    }
+               } else {
+                  _conversions[i] = (x,o) => o;
+               }
 
-                }
-
-                for (var i = 0; i < _fieldCount; i++) {
-                    if (reader.IsDBNull(i))
-                        continue;
-                    if (_errors[i]) {
-                        var value = reader.GetValue(i);
-                        try {
-                            row[fields[i]] = fields[i].Type == "object" ? value : _conversions[i](value);
-                        } catch (FormatException) {
-                            _context.Error($"Could not convert value {value} in field {fields[i].Alias} to {fields[i].Type}");
-                        }
-
-                    } else {
-                        row[fields[i]] = reader.GetValue(i);
-                    }
-                }
-            } else {
-                for (var i = 0; i < _fieldCount; i++) {
-                    if (reader.IsDBNull(i))
-                        continue;
-                    if (_errors[i]) {
-                        row[fields[i]] = fields[i].Type == "object" ? reader.GetValue(i) : _conversions[i](reader.GetValue(i));
-                    } else {
-                        row[fields[i]] = reader.GetValue(i);
-                    }
-                }
             }
 
-            return row;
-        }
+            for (var i = 0; i < _fieldCount; i++) {
+               if (reader.IsDBNull(i))
+                  continue;
+               if (_errors[i]) {
+                  var value = reader.GetValue(i);
+                  try {
+                     row[fields[i]] = fields[i].Type == "object" ? value : _conversions[i](i, value);
+                  } catch (FormatException) {
+                     _context.Error($"Could not convert value {value} in field {fields[i].Alias} to {fields[i].Type}");
+                  }
 
-    }
+               } else {
+                  row[fields[i]] = reader.GetValue(i);
+               }
+            }
+         } else {
+            for (var i = 0; i < _fieldCount; i++) {
+               if (reader.IsDBNull(i))
+                  continue;
+               if (_errors[i]) {
+                  row[fields[i]] = fields[i].Type == "object" ? reader.GetValue(i) : _conversions[i](i, reader.GetValue(i));
+               } else {
+                  row[fields[i]] = reader.GetValue(i);
+               }
+            }
+         }
+
+         return row;
+      }
+
+   }
 }
