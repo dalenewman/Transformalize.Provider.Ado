b9260f9a289a52d3883534bf3673b3d595fe9b2c
dalenewman <dalenewman@gmail.com>
2022-06-29 22:24:07 -0400

    general maintenance

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -4,8 +4,8 @@ REM nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\t
 REM nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules" 
 REM nuget pack Transformalize.Transform.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
  
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.8.36-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.8.36-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
  
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.8.36-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.8.36-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.10.0-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
