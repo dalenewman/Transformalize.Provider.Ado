34ce42d6f8cfc91b57f26232caffc5d087493030
dalenewman <dalenewman@gmail.com>
2022-07-09 15:06:42 -0400

    bump transformalize to 0.10.1 for new properties

--- src/Test.Unit/Properties/AssemblyInfo.cs
+++ src/Test.Unit/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("bd6d534f-48cf-48ff-9c6c-7af0aea661b6")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.10.1.0")]
+[assembly: AssemblyFileVersion("0.10.1.0")]
