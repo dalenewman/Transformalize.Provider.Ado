847dae396da99a5e9be4140111140776e8542516
Dale Newman <dalenewman@gmail.com>
2025-04-20 23:05:17 -0400

    - forgot nuget script versions

--- nuget/myget.bat
+++ nuget/myget.bat
@@ -4,8 +4,8 @@ nuget pack Transformalize.Provider.Ado.Autofac.nuspec -OutputDirectory "c:\temp\
 nuget pack Transformalize.Transform.Ado.nuspec -OutputDirectory "c:\temp\modules" 
 nuget pack Transformalize.Transform.Ado.Autofac.nuspec -OutputDirectory "c:\temp\modules" 
  
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Provider.Ado.Autofac.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
  
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.11.1-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
+nuget push "c:\temp\modules\Transformalize.Transform.Ado.Autofac.0.11.2-beta.nupkg" -source https://www.myget.org/F/transformalize/api/v3/index.json
